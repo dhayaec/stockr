08deaf54037b7a5d5c1a1e55d133104f52d9b0ea
Dhayanandhan <dhayaec@gmail.com>
2022-03-29 12:49:54 +0530

    add more tests

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,6 +1,10 @@
-import { reverseStr } from './utils'
+import { add, reverseStr } from './utils'
 
 describe('utils', () => {
+  test('should add two numbers', () => {
+    expect(add(1, 2)).toEqual(3)
+  })
+
   test('reverseStr should reverse string', () => {
     expect(reverseStr('hello')).toEqual('olleh')
   })
