5c534df7e37ac59c8429cf62ee78b5397760702e
Dhayanandhan <dhayaec@gmail.com>
2022-03-30 19:08:06 +0530

    fix margins

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,8 +6,8 @@ export default function _404() {
     <div>
       <MetaHeader title='Not Found' />
       <div className='flex flex-col justify-center px-2'>
-        <div className='items-center text-4xl text-red-500'>404</div>
-        <div className='ml-2 items-center'>
+        <div className='text-4xl text-red-500'>404</div>
+        <div>
           <p>Oops! The page is not found.</p>
         </div>
       </div>
