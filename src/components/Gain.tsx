83fd250e9da09425695700e2519ef3c726af927a
Dhayanandhan <dhayaec@gmail.com>
2022-03-30 19:57:10 +0530

    check prettier

--- src/components/Gain.tsx
+++ src/components/Gain.tsx
@@ -33,42 +33,42 @@ export class Gain extends Component<GainProps> {
     const { name } = this.props
     const { oldPrice, newPrice, result, isGain } = this.state
     return (
-      <div className="my-2 text-center">
-        <div className="my-1">
+      <div className='my-2 text-center'>
+        <div className='my-1'>
           <h1>{name}</h1>
         </div>
-        <div className="flex justify-center text-center">
-          <div className="w-1/3">
+        <div className='flex justify-center text-center'>
+          <div className='w-1/3'>
             <input
               value={oldPrice || ''}
-              type="number"
-              name="oldPrice"
-              id="oldPrice"
+              type='number'
+              name='oldPrice'
+              id='oldPrice'
               onChange={this.onChange}
-              placeholder="Old Price"
-              className="w-full"
-              pattern="\d*"
+              placeholder='Old Price'
+              className='w-full'
+              pattern='\d*'
             />
           </div>
-          <div className="w-1/3">
+          <div className='w-1/3'>
             <input
               value={newPrice || ''}
-              type="number"
-              name="newPrice"
-              id="newPrice"
+              type='number'
+              name='newPrice'
+              id='newPrice'
               onChange={this.onChange}
-              placeholder="New Price"
-              className="w-full"
-              pattern="\d*"
+              placeholder='New Price'
+              className='w-full'
+              pattern='\d*'
             />
           </div>
-          <div className="flex w-1/3 items-center justify-center border-2 border-dashed border-gray-300">
+          <div className='flex w-1/3 items-center justify-center border-2 border-dashed border-gray-300'>
             <p className={`${isGain ? 'text-green-500' : 'text-red-500'}`}>
               {result}
             </p>
           </div>
         </div>
-        <div className="flex justify-center py-1 text-center">
+        <div className='flex justify-center py-1 text-center'>
           <p className={isGain ? 'text-green-500' : 'text-red-500'}>
             {newPrice - oldPrice}
           </p>
