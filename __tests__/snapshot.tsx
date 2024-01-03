710f36980d25508768d022f7a97a4a7c2328219c
dhayaec <dhayaec@gmail.com>
2024-01-03 11:38:07 +0530

    add tests and remove unused

---
+++ __tests__/snapshot.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react'
+import Home from '../pages'
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      pathname: '/',
+    }
+  },
+}))
+
+describe('snapshot home', () => {
+  it('renders homepage unchanged', () => {
+    const { container } = render(<Home />)
+    expect(container).toMatchSnapshot()
+  })
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,10 @@ import { useRouter } from 'next/router'
 import MetaHeader from '../src/components/common/MetaHeader'
 import Gain from '../src/components/Gain'
 import NoLossBuy from '../src/components/NoLossBuy'
-import PossibleGain from '../src/components/PossibleGain/PossibleGain'
+import PossibleGain from '../src/components/PossibleGain'
 import Target from '../src/components/Target'
 import { menuItems, MenuItemType } from '../src/constants/constants'
+import { reverseStr } from '../src/utils/utils'
 
 const Home: NextPage = () => {
   const router = useRouter()
@@ -13,7 +14,10 @@ const Home: NextPage = () => {
 
   return (
     <div>
-      <MetaHeader title={item.name} />
+      <MetaHeader title={reverseStr(item.name)} />
+      <h1 className='text-center' data-testid='heading'>
+        Stockr
+      </h1>
       <div className='px-2 text-center'>
         <Target name='Target / Stop Loss' />
         <Gain name='Gain / Loss' />
